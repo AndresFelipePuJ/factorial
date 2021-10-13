799d7cf6699388d6f686f67e4d47dd8f06d1c7e9
AndresFelipePuJ <69330292+AndresFelipePuJ@users.noreply.github.com>
2021-10-13 10:40:38 -0500

    Add files via upload

---
+++ .github/workflows/asteriscos del numero ascendente y descendente.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x,i,j,c=0,aux=0;
+int y;
+
+    cout << "digite el numero" << endl;
+    cin>>y;
+
+    x=0;
+    for(j=0;j<(2*y);j++){
+     cout<<endl;
+     c++;
+     if(c<=y)
+     {x++;}
+     if(c>y)
+     {x--;}
+    for(i=1;i<=x;i++){
+
+      cout<<"*";
+}}
+
+
+    return 0;
+}
